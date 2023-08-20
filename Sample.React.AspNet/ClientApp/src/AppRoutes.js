095a0304c61d74dab527017559e1e61838e098b3
Guilherme Bley <guilhermecbley@gmail.com>
2023-08-19 21:14:19 -0300

    Login page

--- Sample.React.AspNet/ClientApp/src/AppRoutes.js
+++ Sample.React.AspNet/ClientApp/src/AppRoutes.js
@@ -1,20 +1,25 @@
 import { Counter } from "./components/Counter";
 import { FetchData } from "./components/FetchData";
+import { Login } from "./components/Login";
 import { Home } from "./components/Home";
 
 const AppRoutes = [
-  {
-    index: true,
-    element: <Home />
-  },
-  {
-    path: '/counter',
-    element: <Counter />
-  },
-  {
-    path: '/fetch-data',
-    element: <FetchData />
-  }
+    {
+        index: true,
+        element: <Home />
+    },
+    {
+        path: '/counter',
+        element: <Counter />
+    },
+    {
+        path: '/fetch-data',
+        element: <FetchData />
+    },
+    {
+        path: '/Login',
+        element: <Login />
+    }
 ];
 
 export default AppRoutes;
