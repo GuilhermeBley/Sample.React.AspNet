716eaeac4266922a3b7011ca1261c87117060aee
Guilherme Bley <guilhermecbley@gmail.com>
2023-08-22 22:54:17 -0300

    add auth

---
+++ Sample.React.AspNet/ClientApp/src/Auth/AuthContext.js
@@ -0,0 +1,19 @@
+﻿import React, { createContext, useState, useEffect } from 'react';
+
+import useAuth from './useAuth';
+
+const Context = createContext();
+
+function AuthProvider({ children }) {
+    const {
+        authenticated, loading, handleLogin, handleLogout,
+    } = useAuth();
+
+    return (
+        <Context.Provider value={{ loading, authenticated, handleLogin, handleLogout }}>
+            {children}
+        </Context.Provider>
+    );
+}
+
+export { Context, AuthProvider };
