0db499e1174305ca0b4768f34168de7600cdcf53
Guilherme Bley <guilhermecbley@gmail.com>
2023-08-25 00:03:58 -0300

    rename folder

--- Sample.React.AspNet/ClientApp/src/App.js
+++ Sample.React.AspNet/ClientApp/src/App.js
@@ -4,7 +4,7 @@ import AppRoutes from './AppRoutes';
 import { Layout } from './components/Layout';
 import './custom.css';
 
-import { AuthProvider } from './Context/AuthContext';
+import { AuthProvider } from './Auth/AuthContext';
 
 export default class App extends Component {
     static displayName = App.name;
