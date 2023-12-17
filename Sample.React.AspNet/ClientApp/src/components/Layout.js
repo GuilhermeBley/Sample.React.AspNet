cade981bdc0fc04b021298c1a694afc05fc15934
Guilherme Bley <guilhermecbley@gmail.com>
2023-12-17 00:45:03 -0300

    create Logout #7 - repair bugs

--- Sample.React.AspNet/ClientApp/src/components/Layout.js
+++ Sample.React.AspNet/ClientApp/src/components/Layout.js
@@ -1,5 +1,5 @@
 import { Container } from 'reactstrap';
-import { NavMenu } from './NavMenu';
+import NavMenu from './NavMenu';
 
 const Layout = ({ children }) => {
 
--- Sample.React.AspNet/ClientApp/src/components/NavMenu.js
+++ Sample.React.AspNet/ClientApp/src/components/NavMenu.js
@@ -3,37 +3,35 @@ import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from '
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
 
-const NavMenu = ({ children }) => {
+const NavMenu = () => {
 
-    const [collapsed, setCollapsed] = useState(false);
+    const [collapsed, setCollapsed] = useState(true);
 
     const toggleNavbar = () => {
         setCollapsed(!collapsed);
     }
 
-    render() {
-        return (
-            <header>
-                <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-                    <NavbarBrand tag={Link} to="/">Sample.React.AspNet</NavbarBrand>
-                    <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-                    <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!collapsed} navbar>
-                        <ul className="navbar-nav flex-grow">
-                            <NavItem>
-                                <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-                            </NavItem>
-                        </ul>
-                    </Collapse>
-                </Navbar>
-            </header>
-        );
-    }
+    return (
+        <header>
+            <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
+                <NavbarBrand tag={Link} to="/">Sample.React.AspNet</NavbarBrand>
+                <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+                <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!collapsed} navbar>
+                    <ul className="navbar-nav flex-grow">
+                        <NavItem>
+                            <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                        </NavItem>
+                    </ul>
+                </Collapse>
+            </Navbar>
+        </header>
+    );
 }
 
 export default NavMenu;
